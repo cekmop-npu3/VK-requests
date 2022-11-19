5fc5219c3f62f1c37ad771dfa6e4e754e74551c3
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2022-11-19 13:32:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup as Bs
+from payload import *
+
+
+class Login:
+    def __init__(self, login, password, basic_headers, login_headers, payload, payload2):
+        self.url = 'https://oauth.vk.com/authorize?client_id=6121396&scope=274677727&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1'
+        self.login_url = 'https://login.vk.com/?act=login&soft=1'
+        self.login = login
+        self.password = password
+        self.b_headers = basic_headers
+        self.l_headers = login_headers
+        self.payload = payload
+        self.payload2 = payload2
+        self.payload['email'] = self.login
+        self.payload['pass'] = self.password
+        self.session = requests.Session()
+        self._main()
+
+    def _main(self):
+        response = self.session.get(self.url, headers=self.b_headers)
+        string = str(response.headers.get("set-cookie"))
+        cookie = f'{string[string.index("remixstlid="):].split(";")[0]}; {string[string.index("remixlgck="):].split(";")[0]}'
+        self.l_headers['cookie'] = cookie
+        soup = Bs(response.text, 'lxml')
+        for i in soup.find('div', class_='oauth_form_login').find_all('input')[:5]: self.payload[i.get('name')] = i.get('value')
+        self._login()
+
+    def _token(self, response):
+        soup = Bs(response.text, 'lxml')
+        href_str = str(soup.find('script', type='text/javascript').findNext())
+        try:
+            href = href_str[href_str.index('location.href = "') + len('location.href = "'):href_str.index('"+addr;')]
+            response = self.session.get(href)
+            print(response.url)
+        except ValueError:
+            print('------Incorrect 2fa------')
+            self._2fa(response)
+
+    def _2fa(self, response):
+        if 'https://oauth.vk.com/login?act=authcheck' in response.url:
+            soup = Bs(response.text, 'lxml')
+            href = 'https://oauth.vk.com' + soup.find('div', class_='form_item').find('form').get('action')
+            phone_mask = soup.find('div', class_='form_item').find('div', class_='fi_row').text.split('номер')[1].strip()[:-1]
+            self.payload2['phone_mask'] = phone_mask
+            self.payload2['code'] = input('2fa: ')
+            response = self.session.post(href, data=payload2, headers=self.b_headers)
+        self._token(response)
+
+    def _login(self):
+        response = self.session.post(self.login_url, data=self.payload, headers=self.l_headers)
+        if 'https://oauth.vk.com/authorize' in response.url:
+            self._captcha(response)
+        else: self._2fa(response)
+
+    def _captcha(self, response):
+        soup = Bs(response.text, 'lxml')
+        try:
+            sid = soup.find_all('input')[-3].get('value')
+            try:
+                link = soup.find('img', class_='oauth_captcha').get('src')
+                print(link)
+                key = input('Captcha: ')
+                self.payload['captcha_sid'] = sid
+                self.payload['captcha_key'] = key
+            except AttributeError:
+                print('------Incorrect data------')
+                self.payload['email'] = input('Login: ')
+                self.payload['pass'] = input('Password: ')
+            self._login()
+        except IndexError: self._token(response)
+
+
+login = Login('login', 'password', headers, headers2, payload, payload2)
---
+++ payload.py
@@ -0,0 +1,49 @@
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'sec-ch-ua-platform': 'Windows',
+    'content-type': 'application/x-www-form-urlencoded',
+    'upgrade-insecure-requests': '1',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+}
+headers2 = {
+    'authority': 'login.vk.com',
+    'method': 'POST',
+    'path': '/?act=login&soft=1',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'content-length': '369',
+    'content-type': 'application/x-www-form-urlencoded',
+    'cookie': '',
+    'origin': 'https://oauth.vk.com',
+    'referer': 'https://oauth.vk.com/',
+    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows",
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+}
+payload = {
+    'ip_h': '',
+    'lg_domain_h': '',
+    '_origin': '',
+    'to': '',
+    'expire': '',
+    'email': '',
+    'pass': ''
+}
+payload2 = {
+    'validation_type': '2fa_sms',
+    'phone_mask': '',
+    'code': ''
+}
